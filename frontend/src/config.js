0b565a1df065d5031885d42b04b1c1510fb10d04
Tulasi-Andimeni <24M11MC002@adityauniversity.in>
2025-09-20 11:25:57 +0530

    change the url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,5 @@
-// src/config.js
-export const BASE_URL = "https://scan2ride-backend.onrender.com";
+// // src/config.js
+// export const BASE_URL = "https://scan2ride-backend.onrender.com";
+
+
+export const BASE_URL = "https://srv-d31ema15pdvs7394ds8g.onrender.com";
